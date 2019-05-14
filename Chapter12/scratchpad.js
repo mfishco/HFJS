0fff8c53aa7fcc1c82502ef9314af4da37ab030e
Mitch Fish <mfishco@gmail.com>
2019-05-14 16:38:10 -0600

    Chapter 12 before leaving for the airport

---
+++ Chapter12/scratchpad.js
@@ -0,0 +1,16 @@
+var dog = {
+    name: "Fido",
+    breed: "Mixed",
+    weight: 38
+};
+
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+}
+
+function Duck(type, canFly) {
+    this.type = type;
+    this.canFly = canFly;
+}
